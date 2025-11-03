4d0c7b1ebbc9f8c78dfe5ebf197021f55f5f8169
peter <peter1uhler@gmail.com>
2025-11-03 21:35:26 +0100

    default depth accounts for octahedra

--- globals.js
+++ globals.js
@@ -5,6 +5,7 @@ const zRotationInput = document.getElementById("z-rotation");
 const depthInput = document.getElementById("depth");
 
 var rotation = [0, 0, 0];
-var shapeDepth = 4;
+var shapeDepth = 3;
+
 
 depthInput.value = shapeDepth;
